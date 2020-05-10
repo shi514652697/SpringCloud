c7345ef2ad57199e46fcbbe83a8eacba094b0fde
shi514652697 <ssnegi_4all@yahoo.com>
2020-05-10 14:19:43 -0500

    updated for config server and git repo changes

--- productservice/src/main/java/com/bharath/springcloud/ProductserviceApplication.java
+++ productservice/src/main/java/com/bharath/springcloud/ProductserviceApplication.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableHystrix
 @EnableHystrixDashboard
+
 public class ProductserviceApplication {
 
 	public static void main(String[] args) {
